0314e5b45518726eb8803ef72c5730686d836acb
Khanh Nguyen <dinhkhanh412@gmail.com>
2021-05-12 13:32:44 +0700

    HomePage: add background color, fix swiper images

--- lib/ui/home/homepage.dart
+++ lib/ui/home/homepage.dart
@@ -15,17 +15,19 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: <Widget>[
-        SizedBox(
-          height: 50,
-        ),
-        Container(
-          constraints: BoxConstraints.expand(height: 200),
-          child: imageSwiper(context),
-        ),
-      ],
-    ));
+      body: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 50,
+          ),
+          Container(
+            constraints: BoxConstraints.expand(height: 200),
+            child: imageSwiper(context),
+          ),
+        ],
+      ),
+      backgroundColor: Color(0xffF5FDFB),
+    );
   }
 }
 
@@ -34,8 +36,14 @@ var images = ['images/plant_1.jpg', 'images/plant_2.jpg', 'images/plant_3.jpg'];
 Swiper imageSwiper(context) {
   return new Swiper(
     autoplay: true,
+    autoplayDelay: 5000,
     itemBuilder: (BuildContext context, int index) {
-      return new Image.asset(images[index]);
+      return new ClipRRect(
+          borderRadius: BorderRadius.circular(10.0),
+          child: Image.asset(
+            images[index],
+            fit: BoxFit.fill,
+          ));
     },
     itemCount: images.length,
     viewportFraction: 0.8,
