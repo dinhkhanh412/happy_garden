689cf7b71b2efc503eb29ac5d4fe0a262590e80d
Khanh Nguyen <dinhkhanh412@gmail.com>
2021-08-04 11:33:42 +0700

    display data in Homepage

--- lib/ui/page/home/homepage.dart
+++ lib/ui/page/home/homepage.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:custom_switch/custom_switch.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:happy_garden/manage/mqtt/MQTTAppState.dart';
 
 import 'package:happy_garden/ui/page/home/widget/ImageSwiper.dart';
@@ -16,6 +17,7 @@ import 'package:provider/provider.dart';
 class HomeScreen extends StatefulWidget {
   final String UID;
   final String gardenName;
+
   HomeScreen({Key key, this.UID, this.gardenName}) : super(key: key);
 
   @override
@@ -28,6 +30,7 @@ class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
   bool online = false;
   bool status = false;
+  bool isLoading = true;
 
   bool connectivity = false;
   num temperature = 0;
@@ -39,6 +42,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     _initValue();
+    _configureAndConnect();
     _manager_1.addListener(() {
       MQTTAppState map1 = _manager_1.currentState;
       Map<String, dynamic> adaResponse = jsonDecode(map1.getReceivedText);
@@ -109,11 +113,165 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    if (isLoading) {
+      return MaterialApp(
+          title: "This is a StatafulWidget",
+          home: Scaffold(
+              body: Align(
+                alignment: Alignment.center,
+                child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  CircularProgressIndicator(),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Please wait....",
+                    style: GoogleFonts.mulish(
+                      color: Colors.green,
+                      fontSize: 18.0,
+                    ),
+                  )
+                ]),
+              )));
+    }
     //_manager.subScribeTo(_topicTextController.text);
     return LayoutBuilder(builder: (context, constraints) {
       return Scaffold(
         backgroundColor: Color(0xffF5FDFB),
-        body: _buildScroll(_manager_1, constraints),
+        body: SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minHeight: constraints.maxHeight,
+            ),
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: constraints.maxHeight * 0.085,
+                ),
+                Row(
+                  children: [
+                    SizedBox(
+                      width: constraints.maxWidth * 0.075,
+                    ),
+                    Text(
+                      "Hello user",
+                      style: TextStyle(fontSize: 24, fontFamily: "Mulish"),
+                    ),
+                    SizedBox(
+                      width: constraints.maxWidth * 0.1,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: constraints.maxHeight * 0.025,
+                ),
+                Container(
+                  constraints: BoxConstraints.expand(height: constraints.maxHeight * 0.25),
+                  child: imageSwiper(context, constraints),
+                ),
+                SizedBox(
+                  height: constraints.maxHeight * 0.05,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        displayToastMessage("Humidity", context);
+                      },
+                      child: Container(
+                        constraints: BoxConstraints.expand(
+                            height: constraints.maxWidth * 0.3, width: constraints.maxWidth * 0.3),
+                        child: card(context, "Humidity", Icons.cloud, humidity.toString()),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        displayToastMessage("Temperature", context);
+                      },
+                      child: Container(
+                        constraints: BoxConstraints.expand(
+                            height: constraints.maxWidth * 0.3, width: constraints.maxWidth * 0.3),
+                        child: card(context, "Temperature", Icons.thermostat_sharp, temperature.toString()),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        displayToastMessage("Water Level", context);
+                      },
+                      child: Container(
+                        constraints: BoxConstraints.expand(
+                            height: constraints.maxWidth * 0.3, width: constraints.maxWidth * 0.3),
+                        child: card(context, "Water Level", Icons.eco, waterLv.toString()),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: constraints.maxHeight * 0.01,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  //Center Row contents horizontally,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        displayToastMessage("Connectivity", context);
+                      },
+                      child: Container(
+                        constraints: BoxConstraints.expand(
+                            height: constraints.maxWidth * 0.3, width: constraints.maxWidth * 0.3),
+                        child: card(context, "Connectivity", Icons.network_wifi, connectivity ? "Online" : "Offline"),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        displayToastMessage("Light Status", context);
+                      },
+                      child: Container(
+                          constraints: BoxConstraints.expand(
+                              height: constraints.maxWidth * 0.3, width: constraints.maxWidth * 0.6),
+                          child: light(lightLv)),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: constraints.maxHeight * 0.01,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  //Center Row contents horizontally,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        displayToastMessage("Status", context);
+                      },
+                      child: Container(
+                        constraints: BoxConstraints.expand(
+                            height: constraints.maxWidth * 0.3, width: constraints.maxWidth * 0.6),
+                        child: cardStat(context, "Light Status", Icons.lightbulb, "OFF"),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        displayToastMessage("Alarm", context);
+                      },
+                      child: Container(
+                        constraints: BoxConstraints.expand(
+                            height: constraints.maxWidth * 0.3, width: constraints.maxWidth * 0.3),
+                        child: card(context, "Alarm", Icons.alarm, "OFF"),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
         bottomNavigationBar: BottomNavigationBar(
           items: const <BottomNavigationBarItem>[
             BottomNavigationBarItem(
@@ -137,143 +295,6 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
-  Widget _buildScroll(MQTTManager manager, BoxConstraints constraints) {
-    return SingleChildScrollView(
-      child: ConstrainedBox(
-        constraints: BoxConstraints(
-          minHeight: constraints.maxHeight,
-        ),
-        child: _buildColumn(manager, constraints),
-      ),
-    );
-  }
-
-  Widget _buildColumn(MQTTManager manager, BoxConstraints constraints) {
-    return Column(
-      children: <Widget>[
-        SizedBox(
-          height: constraints.maxHeight * 0.085,
-        ),
-        Row(
-          children: [
-            SizedBox(
-              width: constraints.maxWidth * 0.075,
-            ),
-            Text(
-              "Hello user",
-              style: TextStyle(fontSize: 24, fontFamily: "Mulish"),
-            ),
-            SizedBox(
-              width: constraints.maxWidth * 0.1,
-            ),
-          ],
-        ),
-        SizedBox(
-          height: constraints.maxHeight * 0.025,
-        ),
-        Container(
-          constraints: BoxConstraints.expand(height: constraints.maxHeight * 0.25),
-          child: imageSwiper(context, constraints),
-        ),
-        SizedBox(
-          height: constraints.maxHeight * 0.05,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            GestureDetector(
-              onTap: () {
-                displayToastMessage("Humidity", context);
-              },
-              child: Container(
-                constraints: BoxConstraints.expand(
-                    height: constraints.maxWidth * 0.3, width: constraints.maxWidth * 0.3),
-                child: card(context, "Humidity", Icons.cloud, "OFF"),
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                displayToastMessage("Temperature", context);
-              },
-              child: Container(
-                constraints: BoxConstraints.expand(
-                    height: constraints.maxWidth * 0.3, width: constraints.maxWidth * 0.3),
-                child: card(context, "Temperature", Icons.thermostat_sharp, "OFF"),
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                displayToastMessage("Water Level", context);
-              },
-              child: Container(
-                constraints: BoxConstraints.expand(
-                    height: constraints.maxWidth * 0.3, width: constraints.maxWidth * 0.3),
-                child: card(context, "Water Level", Icons.eco, "OFF"),
-              ),
-            ),
-          ],
-        ),
-        SizedBox(
-          height: constraints.maxHeight * 0.01,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center, //Center Row contents horizontally,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            GestureDetector(
-              onTap: () {
-                displayToastMessage("Connectivity", context);
-              },
-              child: Container(
-                constraints: BoxConstraints.expand(
-                    height: constraints.maxWidth * 0.3, width: constraints.maxWidth * 0.3),
-                child: card(context, "Connectivity", Icons.network_wifi, "OFF"),
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                displayToastMessage("Light Status", context);
-              },
-              child: Container(
-                  constraints: BoxConstraints.expand(
-                      height: constraints.maxWidth * 0.3, width: constraints.maxWidth * 0.6),
-                  child: light(lightLv)),
-            ),
-          ],
-        ),
-        SizedBox(
-          height: constraints.maxHeight * 0.01,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center, //Center Row contents horizontally,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            GestureDetector(
-              onTap: () {
-                displayToastMessage("Status", context);
-              },
-              child: Container(
-                constraints: BoxConstraints.expand(
-                    height: constraints.maxWidth * 0.3, width: constraints.maxWidth * 0.6),
-                child: cardStat(context, "Light Status", Icons.lightbulb, "OFF"),
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                displayToastMessage("Alarm", context);
-              },
-              child: Container(
-                constraints: BoxConstraints.expand(
-                    height: constraints.maxWidth * 0.3, width: constraints.maxWidth * 0.3),
-                child: card(context, "Alarm", Icons.alarm, "OFF"),
-              ),
-            ),
-          ],
-        ),
-      ],
-    );
-  }
 
   Widget light(num text) {
     return card(context, "Light Status", Icons.lightbulb, text.toString());
@@ -289,14 +310,15 @@ class _HomeScreenState extends State<HomeScreen> {
   void _configureAndConnect() async {
     String server1 = 'server_1';
     String server2 = 'server_2';
-    await _manager_1.initializeMQTTClient(identifier: server1, server: "BBC");
-    await _manager_2.initializeMQTTClient(identifier: server2, server: "BBC1");
+    await _manager_1.initializeMQTTClient(identifier: server1, server: "dinhkhanh412");
+    await _manager_2.initializeMQTTClient(identifier: server2, server: "dinhkhanh412");
     _manager_1.connect();
     _manager_2.connect();
   }
 
   void _initValue() async {
     DeviceAPI deviceAPI = new DeviceAPI(widget.UID);
+
     // temp + humi
     Feed temp = await deviceAPI.getDevice("7");
     String data = temp.data;
@@ -305,6 +327,7 @@ class _HomeScreenState extends State<HomeScreen> {
       temperature = int.parse(data.substring(0, sub));
       humidity = int.parse(data.substring(sub + 1, data.length));
     });
+
     // soil
     temp = await deviceAPI.getDevice("9");
     setState(() {
@@ -322,6 +345,16 @@ class _HomeScreenState extends State<HomeScreen> {
     setState(() {
       pumpStart = (temp.data == "1") ? true : false;
     });
+
+    //Get all parameter
+    dynamic allDevice = await deviceAPI.getAllDevice(widget.UID);
+    if (allDevice.toString() == "None") {
+      setState(() {
+        connectivity = true;
+        isLoading = false;
+      });
+      return;
+    }
   }
 }
 
---
+++ lib/ui/page/welcome_page.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+          backgroundColor: Color(0xff0C9359),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: constraints.maxHeight * 0.4),
+              Text("Welcome to ",
+                  style: GoogleFonts.mulish(
+                      textStyle: TextStyle(color: Colors.white), fontSize: 26, fontWeight: FontWeight.w900)),
+              Text("Happy Garden",
+                  style: GoogleFonts.mulish(
+                      textStyle: TextStyle(color: Colors.white), fontSize: 32, fontWeight: FontWeight.w900)),
+              SizedBox(height: 50),
+              TextButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                  minimumSize: MaterialStateProperty.all<Size>(Size(constraints.maxWidth * 0.95, 50)),
+                ),
+                onPressed: () {},
+                child: Text('TextButton'),
+              )
+            ],
+          ));
+    });
+
+    throw UnimplementedError();
+  }
+}
